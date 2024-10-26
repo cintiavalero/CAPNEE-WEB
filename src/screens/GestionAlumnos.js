46a038db90190d219a4befd90228e66c6843f706
Franco Borsella <fborsella1@gmail.com>
2024-10-26 20:28:46 -0300

    Se hizo el GET alumnos y alta de alumno (no funciona del todo)

--- src/screens/GestionAlumnos.js
+++ src/screens/GestionAlumnos.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Fondo from "../components/FondoB";
 import Navbar from "../components/NavbarVertical";
 import Agregar from "../components/BotonAgregar";
@@ -8,13 +8,36 @@ import iconoFlecha from '../assets/icon-flecha.png';
 import iconoEditar from '../assets/icon-editar.png'; 
 import iconoEliminar from '../assets/icon-trash.png'; 
 import ModalChico from "../components/ModalChico"; 
-import ModalMediano from "../components/ModalMediano"; 
+import ModalMediano from "../components/ModalMediano";
+import AlumnoCard from "../components/AlumnoCard";
 import colors from "../constants/colors";
 import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
+const API_URL = 'http://149.50.140.55:8081';
 
 function GestionAlumnos() {
+    const [alumnos, setAlumnos] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    //Valores del formulario de alta
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [nombreUsuario, setNombreUsuario] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    const [dni, setDni] = useState('');
+    const [fechanacimiento, setFechanacimiento] = useState('');
+    const [errors, setErrors] = useState({});
+
+
+    //Navegar al perfil de un alumno
     const navigate = useNavigate();
+
+    const verPerfil = (e) => {
+      e.preventDefault();
+      navigate('/perfil');
+    }
 
     const [popupEditar, setPopupEditar] = useState(false);    
     const editar = () => { setPopupEditar(true); };
@@ -28,7 +51,75 @@ function GestionAlumnos() {
     const agregar = () => { setPopupAgregar(true); };
     const cancelarAgregar = () => { setPopupAgregar(false); };
     
+    //Recuperar token
+    const token = localStorage.getItem('token');
 
+    //Petición para obtener alumnos asociados a un curso (CAMBIAR ENDPOINT, ESTE TRAE A TODOS LOS USUARIOS)
+    const getAlumnos = async () => {
+      try {
+        const response = await axios.get(`${API_URL}/person/get-all`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        setAlumnos(response.data);
+        console.log(response.data);
+        setLoading(false);
+      } catch (error) {
+        setError('Error al cargar los alumnos');
+        setLoading(false);
+      }
+    };
+
+    useEffect(() => {
+      getAlumnos();
+    }, []);
+
+    //Validar los campos
+    const validarCampos = () => {
+      const newErrors = {}
+      if (!nombre.trim()) newErrors.nombre = "El nombre es obligatorio";
+      if (!apellido.trim()) newErrors.apellido = "El apellido es obligatorio";
+      if (!nombreUsuario.trim()) newErrors.nombreUsuario = "El nombre de usuario es obligatorio";
+      if (!contrasena.trim()) newErrors.contrasena = "La contraseña es obligatoria";
+      if (!dni.trim() || isNaN(dni)) newErrors.dni = "DNI es obligatorio y debe ser numérico";
+      if (!fechanacimiento.trim()) newErrors.fechanacimiento = "La fecha de nacimiento es obligatoria";
+      setErrors(newErrors);
+      return Object.keys(newErrors).length === 0;
+
+    }
+
+    //Petición para alta de Alumno
+    const altaAlumno = async (e) => {
+      e.preventDefault();
+
+      //Validar los campos
+      if (!validarCampos()) return;
+
+      const alumnoData = {
+        name: nombre,
+        lastName: apellido,
+        username: nombreUsuario,
+        password: contrasena,
+        dni: dni,
+        birthdate: fechanacimiento, 
+        rol: "ROLE_STUDENT",
+        //HARDCODEADO POR AHORA
+        courseId: 1
+      };
+
+      try {
+        const response = await axios.post(`${API_URL}/person/add`, alumnoData, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        console.log('Alumno creado: ', response.data);
+        getAlumnos();
+      } catch (error) {
+        console.log('Error al crear el alumno: ', error);
+      }
+    }
 
     return (
         <Fondo >
@@ -39,32 +130,18 @@ function GestionAlumnos() {
                 <h1 className="titulo">Gestión de alumnos: 1°A - 2024</h1>
                 <div className="contenido">
                     <div className="listaAlumnos">
-                        <article className="alumno">
-                            <div className="datosAlumno">
-                                <div className="datosUsuarioAlumno">
-                                    <p><b>Franco Borsella</b></p>
-                                    <small>Usuario: fborsella</small>
-                                </div>
-                                <div className="datosPersonalesAlumno">
-                                    <p><b>DNI:</b>12312312</p>
-                                    <p><b>Nacimiento:</b>01/10/2000</p>
-                                </div>
-                            </div>
-                            <div className="accionesAlumno">
-                                <button onClick={() => navigate('/perfil')}>
-                                    <img className="icono" src={iconoFlecha} alt="Ícono flecha" /> 
-                                    Perfil
-                                </button>
-                                <button onClick={editar}>
-                                    <img className="icono" src={iconoEditar} alt="Ícono editar" /> 
-                                    Modificar
-                                </button>
-                                <button onClick={eliminar}>
-                                    <img className="icono" src={iconoEliminar} alt="Ícono eliminar" />                                     
-                                    Desvincular
-                                </button>
-                            </div>
-                        </article>
+                      {loading ? <p>Cargando alumnos</p> : <p> </p>}
+                      {alumnos.map((alumno) => (
+                        <AlumnoCard
+                          nombre={alumno.name + ' ' + alumno.lastName}
+                          usuario={alumno.username}
+                          dni={alumno.dni}
+                          fechaNacimiento={alumno.birthdate}
+                          handleVerAlumno={verPerfil}
+                          handleEditar={editar}
+                          handleEliminar={eliminar}
+                        />
+                      ))}
                     </div>
                     <Agregar onClick={agregar}/>
                 </div>
@@ -110,24 +187,31 @@ function GestionAlumnos() {
             )}
             {popupAgregar && (
               <ModalMediano 
-                titulo="Agregar un curso nuevo"
+                titulo="Agregar un alumno nuevo"
                 cerrar={cancelarAgregar} 
                 colorFondo={colors.violeta}
+                aceptar={altaAlumno}
               >
                 <div className="bodyModal">
                   <div id="headAlumno" className="introduccion">
-                    <p>Para agregar un curso complete los siguientes datos:</p>
+                    <p>Para agregar un alumno complete los siguientes datos:</p>
                   </div>
                   <form id="formAlumno">
                     <div>
-                        <input placeholder="Nombre"/>
-                        <input placeholder="Usuario"/>
-                        <input placeholder="DNI"/>
+                        <input placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required/>
+                        {errors.nombre && <p style={{ fontSize: '9px', color: 'red' }}>{errors.nombre}</p>}
+                        <input placeholder="Usuario" value={nombreUsuario} onChange={(e) => setNombreUsuario(e.target.value)} required/>
+                        {errors.nombreUsuario && <p style={{ fontSize: '9px', color: 'red' }}>{errors.nombreUsuario}</p>}
+                        <input placeholder="DNI" value={dni} onChange={(e) => setDni(e.target.value)} required/>
+                        {errors.dni && <p style={{ fontSize: '9px', color: 'red' }}>{errors.dni}</p>}
                     </div>
                     <div>
-                        <input placeholder="Apellido"/>
-                        <input placeholder="Contraseña"/>
-                        <input placeholder="dd/mm/aa"/>
+                        <input placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} required/>
+                        {errors.apellido && <p style={{ fontSize: '9px', color: 'red' }}>{errors.apellido}</p>}
+                        <input placeholder="Contraseña" value={contrasena} onChange={(e) => setContrasena(e.target.value)} required/>
+                        {errors.contrasena && <p style={{ fontSize: '9px', color: 'red' }}>{errors.contrasena}</p>}
+                        <input placeholder="aa/mm/dd" value={fechanacimiento} onChange={(e) => setFechanacimiento(e.target.value)} required/>
+                        {errors.fechanacimiento && <p style={{ fontSize: '9px', color: 'red' }}>{errors.fechanacimiento}</p>}
                     </div>
                   </form>
                 </div>
