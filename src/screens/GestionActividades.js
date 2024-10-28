619c24bc03cd3f553762ac6bdc8246ac2c746b76
Franco Borsella <fborsella1@gmail.com>
2024-10-28 20:06:58 -0300

    Se hizo petición para obtener todas las actividades

--- src/screens/GestionActividades.js
+++ src/screens/GestionActividades.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Fondo from "../components/FondoB";
 import Navbar from "../components/NavbarVertical";
+import ActividadCard from "../components/ActividadCard";
 import "./general.css";
 import "./Styles/GestionActividades.css";
-import iconoEjercicio from '../assets/icon-book.png'; 
-import { useNavigate } from 'react-router-dom';
+import iconoEjercicio from '../assets/icon-book.png';
 
+import { useNavigate, useParams } from 'react-router-dom';
+import axios from "axios";
+
+const API_URL = 'http://149.50.140.55:8082';
 
 function GestionActividades() {
+    const [nombreSubBloque, setNombreSubBloque] = useState('');
+    const [actividades, setActividades] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);    
     const navigate = useNavigate();
+
+    //Recuperar parámetros de ruta
+    const { idCurso, idSubBloque } = useParams();
+
+    //Recuperar token
+    const token = localStorage.getItem('token');
+
+    //Petición para obtener actividades
+    const getActividades = async () => {
+        try {
+            const response = await axios.get(`${API_URL}/thematic-content/get-all-by-thematic-subblock-id-and-course-id?thematicSubblockId=${idSubBloque}&courseId=${idCurso}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            setActividades(response.data.thematicContents);
+            setNombreSubBloque(response.data.thematicSubblock);
+            setLoading(false);
+        } catch (error) {
+            setError('Error al cargar las actividades');
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        getActividades();
+    }, []);
 
     return (
         <Fondo >
@@ -17,16 +52,18 @@ function GestionActividades() {
             </div>
             <body className="gestionActividades">
                 <h1 className="titulo">Administración de actividades del curso</h1>
-                <h2>Usar y conocer los números: Selecione una actividad</h2>
+                <h2><b>{nombreSubBloque}:</b> Selecione una actividad</h2>
                 <div className="contenido">
                     <div className="listaActividades">
-                        <article className="actividad" onClick={() => navigate('/gestionejercicios')}>
-                            <p>Contextos y uso social de los números</p>
-                            <div className="cantidadEjercicios">
-                                <img src={iconoEjercicio} alt="Icono de libro"/> 
-                                <p>Ejercicios: 3</p>
-                            </div>
-                        </article>
+                        {loading ? <p>Cargando Actividades</p> : <p> </p>}
+                        {actividades.map((actividad) => (
+                            <ActividadCard
+                                key={actividad.id}
+                                titulo={actividad.name}
+                                rutaDestino={`/gestionejercicios/curso/${idCurso}/actividad/${actividad.id}`}
+                                cantidadEjercicios={actividad.numberOfAssociatedExercises}
+                            />
+                        ))}
                     </div>
                 </div>
             </body>
