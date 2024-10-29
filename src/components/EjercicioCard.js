a8a5bf21eab064873554f76d809cbaf9bc460b5a
Franco Borsella <fborsella1@gmail.com>
2024-10-29 14:46:09 -0300

    fix: Se arregló el desborde en la calificación automática del ejercicio

--- src/components/EjercicioCard.js
+++ src/components/EjercicioCard.js
@@ -3,10 +3,10 @@ import iconoFlecha from '../assets/icon-flecha.png';
 import iconoEditar from '../assets/icon-editar.png'; 
 import iconoEliminar from '../assets/icon-trash.png'; 
 
-export default function CartaEjercicio({nombre, fecha = 'DD/MM/AAAA', handleVerIntentos, handleEditar, handleEliminar}) {
+export default function CartaEjercicio({nombre, fecha, handleVerIntentos, handleEditar, handleEliminar}) {
     return (
         <article className="ejercicio">
-            <p>{nombre} - {fecha}</p>
+            <p>{nombre} {fecha}</p>
             <div className="accionesEjercicio">
                 <button onClick={handleVerIntentos}>
                     <img className="icono" src={iconoFlecha} alt="Ícono flecha" /> 
